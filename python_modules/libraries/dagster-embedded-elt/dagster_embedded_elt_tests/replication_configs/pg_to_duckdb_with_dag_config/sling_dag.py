79818bc47a6a9f039a07a66d2e3abd52d3da3df3
Pedram Navid <1045990+PedramNavid@users.noreply.github.com>
2024-02-22 18:41:50 -0800

    [embedded-elt] Deprecate Sling factory method and SlingSource and SlingTarget Resources

--- python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt_tests/replication_configs/pg_to_duckdb_with_dag_config/sling_dag.py
+++ python_modules/libraries/dagster-embedded-elt/dagster_embedded_elt_tests/replication_configs/pg_to_duckdb_with_dag_config/sling_dag.py
@@ -1,6 +1,16 @@
-from dagster import Definitions, file_relative_path
-from dagster_embedded_elt.sling import DagsterSlingTranslator, sling_assets
-from dagster_embedded_elt.sling.resources import SlingConnectionResource, SlingResource
+from dagster import AssetSpec, Definitions, file_relative_path
+from dagster_embedded_elt.sling import (
+    DagsterSlingTranslator,
+    SlingMode,
+    build_sling_asset,
+    sling_assets,
+)
+from dagster_embedded_elt.sling.resources import (
+    SlingConnectionResource,
+    SlingResource,
+    SlingSourceConnection,
+    SlingTargetConnection,
+)
 
 replication_config = file_relative_path(__file__, "sling_replication.yaml")
 
@@ -19,6 +29,14 @@ sling_resource = SlingResource(
     ]
 )
 
+asset_deprecated = build_sling_asset(
+    asset_spec=AssetSpec(key=["main", "dest_tbl"]),
+    source_stream="file:///tmp/test.csv",
+    target_object="main.dest_table",
+    mode=SlingMode.INCREMENTAL,
+    primary_key="id",
+)
+
 
 @sling_assets(replication_config=replication_config)
 def my_assets(context, sling: SlingResource):
@@ -30,7 +48,17 @@ def my_assets(context, sling: SlingResource):
         context.log.info(row)
 
 
+sling_other_resource = SlingResource(
+    source_connection=SlingSourceConnection(
+        type="postgres",
+        connection_string="postgres://postgres:postgres@localhost:5432/finance?sslmode=disable",
+    ),
+    target_connection=SlingTargetConnection(
+        type="duckdb", connection_string="duckdb:///var/tmp/duckdb.db"
+    ),
+)
+
 defs = Definitions(
-    assets=[my_assets],
-    resources={"sling": sling_resource},
+    assets=[my_assets, asset_deprecated],
+    resources={"sling": sling_resource, "other": sling_other_resource},
 )
