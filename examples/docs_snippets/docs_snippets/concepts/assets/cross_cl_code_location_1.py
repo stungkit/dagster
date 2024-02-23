f9233eec624d112d384d1fb53a519957816a4b6a
JamieDeMaria <jamie@dagsterlabs.com>
2024-02-23 16:10:32 -0500

    fix tests

--- examples/docs_snippets/docs_snippets/concepts/assets/cross_cl_code_location_1.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/cross_cl_code_location_1.py
@@ -6,7 +6,7 @@ from dagster import Definitions, asset
 
 @asset
 def code_location_1_asset():
-    with open("data/code_location_1_asset.json", "w") as f:
+    with open("data/code_location_1_asset.json", "w+") as f:
         json.dump(5, f)
 
 
--- examples/docs_snippets/docs_snippets/concepts/assets/cross_cl_code_location_2.py
+++ examples/docs_snippets/docs_snippets/concepts/assets/cross_cl_code_location_2.py
@@ -9,10 +9,10 @@ code_location_1_source_asset = SourceAsset(key=AssetKey("code_location_1_asset")
 
 @asset(deps=[code_location_1_source_asset])
 def code_location_2_asset():
-    with open("data/code_location_1_asset.json", "w") as f:
+    with open("data/code_location_1_asset.json", "r") as f:
         x = json.load(f)
 
-    with open("data/code_location_2_asset.json", "w") as f:
+    with open("data/code_location_2_asset.json", "w+") as f:
         json.dump(x + 6, f)
 
 
