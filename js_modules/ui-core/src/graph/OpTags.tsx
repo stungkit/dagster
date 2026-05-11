6893ae16f14335789804f736efaa4a2927dc922d
gibsondan <daniel@elementl.com>
2026-05-11 13:11:30 -0500

    remove dagster-test reference from example docs project (#24193)
    Summary:
    I suspect this was just copy-pasted errantly and does not need to be here.
    
    ## Summary & Motivation
    
    ## Test Plan
    
    ## Changelog
    
    > The changelog is generated by an agent that examines merged PRs and
    > summarizes/categorizes user-facing changes. You can optionally replace
    > this text with a terse description of any user-facing changes in your PR,
    > which the agent will prioritize. Otherwise, delete this section.
    
    Internal-RevId: 8846c4d7890d7d8bd68a3bae816dfe7ab37325c0

--- js_modules/ui-core/src/graph/OpTags.tsx
+++ js_modules/ui-core/src/graph/OpTags.tsx
@@ -21,7 +21,6 @@ import toml from './kindtag-images/toml.svg';
 import airbyte from './kindtag-images/tool-airbyte-color.svg';
 import airflow from './kindtag-images/tool-airflow-color.svg';
 import airtable from './kindtag-images/tool-airtable-color.svg';
-import alteryx from './kindtag-images/tool-alteryx-color.svg';
 import arrow from './kindtag-images/tool-arrow-color.svg';
 import atlan from './kindtag-images/tool-atlan-color.svg';
 import aws from './kindtag-images/tool-aws-color.svg';
@@ -31,7 +30,6 @@ import azure from './kindtag-images/tool-azure-color.svg';
 import azuredevops from './kindtag-images/tool-azuredevops-color.svg';
 import azureml from './kindtag-images/tool-azureml-color.svg';
 import bigquery from './kindtag-images/tool-bigquery-color.svg';
-import boomi from './kindtag-images/tool-boomi-color.svg';
 import botorch from './kindtag-images/tool-botorch-color.svg';
 import braze from './kindtag-images/tool-braze.svg';
 import cassandra from './kindtag-images/tool-cassandra-color.svg';
@@ -172,7 +170,6 @@ import rust from './kindtag-images/tool-rust-color.svg';
 import s3 from './kindtag-images/tool-s3-color.svg';
 import sagemaker from './kindtag-images/tool-sagemaker-color.svg';
 import salesforce from './kindtag-images/tool-salesforce-color.svg';
-import sap from './kindtag-images/tool-sap-color.svg';
 import scala from './kindtag-images/tool-scala-color.svg';
 import scikitlearn from './kindtag-images/tool-scikitlearn-color.svg';
 import scipy from './kindtag-images/tool-scipy-color.svg';
@@ -248,7 +245,6 @@ export type KnownTagType =
   | 'airflow'
   | 'airliftmapped'
   | 'airtable'
-  | 'alteryx'
   | 'arrow'
   | 'athena'
   | 'atlan'
@@ -261,7 +257,6 @@ export type KnownTagType =
   | 'azuredevops'
   | 'azureml'
   | 'bigquery'
-  | 'boomi'
   | 'botorch'
   | 'braze'
   | 'bronze'
@@ -425,7 +420,6 @@ export type KnownTagType =
   | 's3'
   | 'sagemaker'
   | 'salesforce'
-  | 'sap'
   | 'scala'
   | 'scikitlearn'
   | 'scipy'
@@ -617,10 +611,6 @@ export const KNOWN_TAGS: Record<KnownTagType, KnownTag> = {
     icon: airtable,
     content: 'Airtable',
   },
-  alteryx: {
-    icon: alteryx,
-    content: 'Alteryx',
-  },
   omni: {
     icon: omni,
     content: 'Omni',
@@ -678,10 +668,6 @@ export const KNOWN_TAGS: Record<KnownTagType, KnownTag> = {
   bigquery: {
     icon: bigquery,
     content: 'BigQuery',
-  },
-  boomi: {
-    icon: boomi,
-    content: 'Boomi',
   },
   teams: {
     icon: teams,
@@ -1119,10 +1105,6 @@ export const KNOWN_TAGS: Record<KnownTagType, KnownTag> = {
   salesforce: {
     icon: salesforce,
     content: 'Salesforce',
-  },
-  sap: {
-    icon: sap,
-    content: 'SAP',
   },
   sdf: {
     icon: sdf,
